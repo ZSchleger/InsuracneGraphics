928e047051bca016ea546c2a09e7a28427b64019
zach schleger <zachs376@gmail.com>
2023-09-25 13:59:33 -0400

    Added definitions page

---
+++ docs/definitions.js
@@ -0,0 +1,9 @@
+function showGeneral() {
+    document.getElementById('general').style.display = 'block';
+    document.getElementById('accidents').style.display = 'none';
+}
+
+function showAccidents() {
+    document.getElementById('general').style.display = 'none';
+    document.getElementById('accidents').style.display = 'block';
+}
