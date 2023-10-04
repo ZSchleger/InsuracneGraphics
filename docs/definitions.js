04e9e74bcb050ee1da2ca29ba8a8c74f079e9a9a
Zachary Schleger <zachs376@gmail.com>
2023-10-04 16:43:09 -0400

    Added people page

--- docs/definitions.js
+++ docs/definitions.js
@@ -1,9 +1,17 @@
 function showGeneral() {
+    document.getElementById('people').style.display = 'none';
     document.getElementById('general').style.display = 'block';
     document.getElementById('accidents').style.display = 'none';
 }
 
 function showAccidents() {
+    document.getElementById('people').style.display = 'none';
     document.getElementById('general').style.display = 'none';
     document.getElementById('accidents').style.display = 'block';
+}
+
+function showPeople() {
+    document.getElementById('people').style.display = 'block';
+    document.getElementById('general').style.display = 'none';
+    document.getElementById('accidents').style.display = 'none';
 }
